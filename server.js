dd69f970e1ebed4485abea80eb3369a8f9bf4dae
Brian best <brian.brianb64@gmail.com>
2015-02-02 13:20:01 -0400

    init commit

---
+++ server.js
@@ -0,0 +1,15 @@
+/**
+ * Created by brianbest on 15-02-02.
+ */
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/dist')); // Folder for files
+
+app.get('/', function (req, res) {
+    res.send('index.html')
+})
+
+var server = app.listen(3000, function () {
+
+})
